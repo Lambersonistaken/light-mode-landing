18139e28a978c2b2a0bafd25dc6dafd2ec7a2901
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-09-25 17:30:16 +0300

    hero section done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,31 @@
+import ArrowIcon from "@/assets/arrow-right.svg"
+import cogImage from "@/assets/cog.png"
+import cylinderImage from "@/assets/cylinder.png"
+import noodleImage from "@/assets/noodle.png"
+import Image from "next/image"
+
 export const Hero = () => {
   return (
-    <section>
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_96%)] overflow-x-clip">
       <div className="container">
-        <div>
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
           <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here</div>
-          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Pathway to productivity</h1>
+          <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Pathway to productivity</h1>
           <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
-          <div>
-            <button>Get for free</button>
-            <button>Learn more</button>
+          <div className="flex gap-1 items-center mt-[30px]">
+            <button className="btn btn-primary">Get for free</button>
+            <button className="btn btn-text gap-1">
+              <span>Learn more</span>
+              <ArrowIcon className="w-5 h-5 inline-flex justify-center items-center" />
+            </button>
           </div>
+        </div>
+        <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative ">
+          <Image src={cogImage} alt="Cog" className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"/>
+          <Image src={cylinderImage} alt="Cylinder" width={220} className="hidden md:block -top-8 -left-32 md:absolute"/>
+          <Image src={noodleImage} alt="Noodle" width={220} className="hidden lg:block lg:absolute top-[524px] left-[448px] rotate-[30deg]"/>
+        </div>
         </div>
       </div>
     </section>
