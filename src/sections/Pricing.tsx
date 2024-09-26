c7d71e16589ce67de0d3491c6d382c7a9d97595b
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-09-26 19:05:35 +0300

    pricing ongoing

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,6 @@
+import CheckIcon from "@/assets/check.svg"
+
+
 const pricingTiers = [
   {
     title: "Free",
@@ -52,6 +55,33 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <section className="py-24">
+      <div className="container">
+        <h2 className="section-title">Pricing</h2>
+        <p className="section-description mt-5">Free forever. Upgrade for unlimited tasks, better security, and exclusive features.</p>
+        <div>
+          {pricingTiers.map(({title, monthlyPrice, buttonText, popular, inverse, features}) => (
+            // eslint-disable-next-line react/jsx-key
+            <div>
+              <h3>{title}</h3>
+              <div>
+                <span>${monthlyPrice}</span>
+                <span>/month</span>
+              </div>
+              <button>{buttonText}</button>
+              <ul>
+                {features.map((feature) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <li>
+                    <CheckIcon className="h-6 w-6" /> 1.26
+                    <span>{feature}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
