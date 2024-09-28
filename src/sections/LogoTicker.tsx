8a41309ae4805e1405580e2b1ba5209ec2d23f94
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-09-28 15:58:47 +0300

    logo animation done

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -14,14 +14,31 @@ export const LogoTicker = () => {
     <div className="py-8 bg-white md:py-12">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-        <div className="flex gap-14 flex-none">
+        <motion.div className="flex gap-14 flex-none pr-14" animate={{
+          translateX: "-50%",
+        }}
+        transition={{
+          duration: 25,
+          repeat: Infinity,
+          ease: "linear",
+          repeatType: "loop"
+
+        }}
+        >
           <Image src={acmeLogo} alt="Acme Logo" className="logo-ticker-image" />
           <Image src={quantumLogo} alt="Quantum Logo" className="logo-ticker-image" />
           <Image src={echoLogo} alt="Echo Logo" className="logo-ticker-image" />
           <Image src={celestialLogo} alt="Celestial Logo" className="logo-ticker-image" />
           <Image src={pulseLogo} alt="Pulse Logo" className="logo-ticker-image" />
           <Image src={apexLogo} alt="Apex Logo" className="logo-ticker-image" />
-        </div>
+          {/* Second Set of Logos */}
+          <Image src={acmeLogo} alt="Acme Logo" className="logo-ticker-image" />
+          <Image src={quantumLogo} alt="Quantum Logo" className="logo-ticker-image" />
+          <Image src={echoLogo} alt="Echo Logo" className="logo-ticker-image" />
+          <Image src={celestialLogo} alt="Celestial Logo" className="logo-ticker-image" />
+          <Image src={pulseLogo} alt="Pulse Logo" className="logo-ticker-image" />
+          <Image src={apexLogo} alt="Apex Logo" className="logo-ticker-image" />
+        </motion.div>
         </div>
       </div>
     </div>
